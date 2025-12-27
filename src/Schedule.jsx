2fbb806706087289d4706698c0e70c03ac840f73
Dave Weissman <weissmad@gmail.com>
2025-12-27 15:39:54 -0500

    added boys and girls track added condition for no results found

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import moment from 'moment';
 
 function Schedule({ error, url, title, matches, showComplete }) {
-  if (error!="") {
+  if (error != "") {
     return (
       <div className="container-fluid">
         <h2 style={{ textAlign: 'center' }}>Unable to load the schedule</h2>
@@ -16,42 +16,57 @@ function Schedule({ error, url, title, matches, showComplete }) {
     return 'Loading...';
   }
 
+  console.dir(matches);
+  let shownMatches = 0;
+
   return (
-    <div className="container-fluid">
-      <h2 style={{ textAlign: 'center' }}>{title}</h2>
-      <div className="card-container">
-        {matches.map((match, cnt) => {
-          let backgroundColor = (match.isDone) ? "#CCC" : "#FFF"
-          let hideTile = showComplete || !match.isDone ? 'block' : 'none';
+    <>
+      <div className="container-fluid">
+        <h2 style={{ textAlign: 'center' }}>{title}</h2>
+        <div className="card-container">
+          {matches.map((match, cnt) => {
+            let backgroundColor = (match.isDone) ? "#CCC" : "#FFF"
+            let hideTile = showComplete || !match.isDone ? 'block' : 'none';
 
-          // set border color based on when date is.  if today, then green.  if tomorrow then yellow, else normal
-          let matchDate=moment(match.date).format('MMMM Do YYYY');
-          let matchDOW=moment(match.date).format('ddd');
-          let nowDate=moment().format('MMMM Do YYYY');
-          let tomorrowDate=moment().add(1, 'days').format('MMMM Do YYYY');
-          let borderHighlightClass="";
-          let textHighlightClass="";
-          if (matchDate===nowDate) {
-            borderHighlightClass="border-success";
-            textHighlightClass="text-success";
-          }else if (matchDate===tomorrowDate) {
-            borderHighlightClass="border-warning";
-            textHighlightClass="text-warning";
-          }
+            // set border color based on when date is.  if today, then green.  if tomorrow then yellow, else normal
+            let matchDate = moment(match.date).format('MMMM Do YYYY');
+            let matchDOW = moment(match.date).format('ddd');
+            let nowDate = moment().format('MMMM Do YYYY');
+            let tomorrowDate = moment().add(1, 'days').format('MMMM Do YYYY');
+            let borderHighlightClass = "";
+            let textHighlightClass = "";
+            if (matchDate === nowDate) {
+              borderHighlightClass = "border-success";
+              textHighlightClass = "text-success";
+            } else if (matchDate === tomorrowDate) {
+              borderHighlightClass = "border-warning";
+              textHighlightClass = "text-warning";
+            }
 
-          return (
-            <div className={`card clearfix ${borderHighlightClass}`} style={{ display: hideTile, backgroundColor, margin: '5px' }} key={cnt}>
-              <div className="card-body">
-                <div className={`card-sub-title ${textHighlightClass}`}>{matchDOW} {match.date} {match.isDone}</div>
-                Venue: {match.facility}<br />
-                {match.awayTeam} @ {match.homeTeam}<br />
+            if (hideTile === 'block') {
+              shownMatches++;
+            }
+
+            return (
+              <div className={`card clearfix ${borderHighlightClass}`} style={{ display: hideTile, backgroundColor, margin: '5px' }} key={cnt}>
+                <div className="card-body">
+                  <div className={`card-sub-title ${textHighlightClass}`}>{matchDOW} {match.date} {match.isDone}</div>
+                  Venue: {match.facility}<br />
+                  {removeHTML(match.awayTeam)} @ {match.homeTeam}<br />
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </div>
-    </div>
+
+      {(!showComplete && shownMatches === 0) && <div>No matches found</div>}
+    </>
   );
+}
+
+const removeHTML = (text) => {
+  return text.replace(/<[^>]*>?/gm, '');
 }
 
 export default Schedule;
